d1f1a322da0d70950f9b491d5b1004edda24052b
Luke Chen <luke.chen.2010@gmail.com>
2020-09-01 01:32:29 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+  
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(({people}) => this.setState({ peopleInSpace: people }))
+  }
+  
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+
+}
+
+export default App
