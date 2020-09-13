126306a2174bb6e01fde24df36cdce13f617d022
rfgr87 <royguzrod@gmail.com>
2020-09-13 18:33:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+
+import React from 'react'
+import { render } from 'enzyme'
+
+export default class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            astrosInSpace: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+            .then(response => response.json())
+            .then((json) => {
+                this.setState({astrosInSpace: json.people})
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.astrosInSpace.map(astro => astro.name)}
+            </div>
+        )
+    }
+
+    
+}
